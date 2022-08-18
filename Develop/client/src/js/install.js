68e968726108087e7962e60238409a01ee843deb
raphson1 <raphsoneric@gmail.com>
2022-08-18 01:44:09 -0500

    asset caching

--- Develop/client/src/js/install.js
+++ Develop/client/src/js/install.js
@@ -9,7 +9,7 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async (event) => {
+butInstall.addEventListener('click', async () => {
     
     const promptEvent = window.deferredPrompt;
     if(!promptEvent){
@@ -17,8 +17,11 @@ butInstall.addEventListener('click', async (event) => {
     }
     promptEvent.prompt();
     window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true)
+    butInstall.classList.toggle('hidden', true);
+
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
